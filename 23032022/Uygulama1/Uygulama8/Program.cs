3487e357757ec431c68a4f1a9ea5f9369a5e6602
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-03-23 20:18:29 +0300

    ibrahimbayramli

--- 23032022/Uygulama1/Uygulama8/Program.cs
+++ 23032022/Uygulama1/Uygulama8/Program.cs
@@ -8,9 +8,26 @@ namespace Uygulama8
 {
     class Program
     {
+        public static float ZamYap(float fiyat)
+        {
+            if (fiyat >= 500 && fiyat < 750) return fiyat *= 1.05f;
+            else return fiyat *= 1.1f;
+        }
+        public static float KucukEvAletleri(float fiyat)
+        {
+            return fiyat * 1.05f + 250;
+        }
+        public static bool IslemeDevamEtsinMi()
+        {
+            Console.Write("Devam etmek istermisiniz? ");
+            string cevap = Console.ReadLine();
+            if (cevap == "evet" || cevap == "EVET" || cevap == "Evet") return true;
+            else return false;
+        }
         public static int Fatura(int fiyat,int kdv)
         {
             fiyat += fiyat * kdv / 100;
+            fiyat=Indirim(fiyat);
             return fiyat;
         }
         public static void HediyeCeki()
@@ -25,6 +42,14 @@ namespace Uygulama8
             {
                 Console.WriteLine("2500TL üzerinde alışveriş yapanları");
             }
+        }
+        public static int Indirim(int fatura)
+        {
+            Console.Write("İndirim tutarını giriniz: ");
+            int indirim = Convert.ToInt32(Console.ReadLine());
+            int fiyat = 0;
+                fiyat-=indirim;
+            return fiyat;
         }
         static void Main(string[] args)
         {
@@ -42,7 +67,7 @@ namespace Uygulama8
                     Console.WriteLine("KDV tutarını hesaplat.");
                     int kdv = Convert.ToInt32(Console.ReadLine());
                     HediyeCeki();
-                    Fatura(fiyat, kdv);
+                    Console.WriteLine(Fatura(fiyat, kdv));
                     break;
 
                 case '2':
@@ -55,13 +80,33 @@ namespace Uygulama8
                         Console.WriteLine("KDV tutarını hesaplat.");
                         int kdv1 = Convert.ToInt32(Console.ReadLine());
                         HediyeCeki();
-                        Fatura(fiyat1, kdv1);
+                        Console.WriteLine(Fatura(fiyat1, kdv1)); 
                     }
                     break;
 
                 case '3':
+                    Console.WriteLine("1- Mutfak aletleri");
+                    Console.WriteLine("2- Küçük ev aletleri");
+                    Console.Write("Bir seçim yapınız: ");
+                    char secenek = Convert.ToChar(Console.ReadLine());
+                    switch (secenek)
+                    {
+                        case '1':
+                            Console.Write("Fiyatı giriniz: ");
+                            float ucret = Convert.ToSingle(Console.ReadLine());
+                            float tutar = ZamYap(ucret);
+                            Console.WriteLine(tutar);
+                            break;
+                        case '2':
+                            Console.Write("Fiyatı giriniz: ");
+                            float a = Convert.ToSingle(Console.ReadLine());
+                            float b = KucukEvAletleri(a);
+                            Console.WriteLine(b);
+                            break;
+                    }
                     break;
             }
+            Console.ReadKey();
         }
     }
 }
---
+++ 23032022/Uygulama1/Uygulama9/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama9
+{
+    class Program
+    {
+        public static float TlUsd(float tutar)
+        {
+            return tutar / 15;
+        }
+        public static float UsdTl(float tutar)
+        {
+            return tutar * 15;
+        }
+        public static float TlEur(float tutar)
+        {
+            return tutar / 19;
+        }
+        public static float EurTl(float tutar)
+        {
+            return tutar *19;
+        }
+        public static float TlAlt(float tutar)
+        {
+            return tutar / 1000;
+        }
+        public static float AltTl(float tutar)
+        {
+            return tutar *1000;
+        }
+        static void Main(string[] args)
+        {
+            float tutar;
+            git:
+            Console.WriteLine("Yapmak istediğiniz işlemi seçiniz:");
+            Console.WriteLine("1- TL-USD");
+            Console.WriteLine("2- USD-TL");
+            Console.WriteLine("3- TL-Euro");
+            Console.WriteLine("4- Euro-TL");
+            Console.WriteLine("5- TL-Altın");
+            Console.WriteLine("6- Altın-TL");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            switch (secim)
+            {
+                case 1:
+                    Console.Write("Tutar giriniz: ");
+                    tutar = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine(TlUsd(tutar));
+                    break;
+                case 2:
+                    Console.Write("Tutar giriniz: ");
+                    tutar = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine(UsdTl(tutar));
+                    break;
+                case 3:
+                    Console.Write("Tutar giriniz: ");
+                    tutar = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine(TlEur(tutar));
+                    break;
+                case 4:
+                    Console.Write("Tutar giriniz: ");
+                    tutar = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine(EurTl(tutar));
+                    break;
+                case 5:
+                    Console.Write("Tutar giriniz: ");
+                    tutar = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine(TlAlt(tutar));
+                    break;
+                case 6:
+                    Console.Write("Tutar giriniz: ");
+                    tutar = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine(AltTl(tutar));
+                    break;
+            }
+            Console.Write("Yeni bir işlem yapmak istermisiniz? <true/false>");
+            bool yeniIslem = Convert.ToBoolean(Console.ReadLine());
+            if (yeniIslem) goto git;
+            Console.ReadKey();
+        }
+    }
+}
