00fed482b1c0afba95b2d1e432924b7906cd9908
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-03-29 20:14:19 +0300

    ibrahimbayramli

--- 29032022/Uygulamalar/Uygulama4/Muhasebe.cs
+++ 29032022/Uygulamalar/Uygulama4/Muhasebe.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Uygulama4
 {
-    class Muhasebe
+    class Muhasebe : Personel
     {
+        public override void PrimHesapla(string statu, int yas, string seans, int ucret)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int SeansUcretiHepla(int seans, int ucret)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
