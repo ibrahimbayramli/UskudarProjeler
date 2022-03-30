cf8394410368ce57812c1b4bc8558967968745ff
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-03-30 17:45:28 +0300

    ibrahimbayramli

---
+++ 30032022/30032022/Uygulama2/Daire.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama2
+{
+    class Daire : Ev
+    {
+        public string cephe;
+        public string daireDurumu;
+        public int katSayisi;
+        public int balkonSayisi;
+        
+        public void EvVerileri()
+        {
+            VeriCek();
+            Console.Write("Cephe bilgisi giriniz: ");
+            cephe = Console.ReadLine();
+            Console.Write("Daire durumu giriniz: ");
+            daireDurumu = Console.ReadLine();
+            Console.Write("Kat sayısını giriniz: ");
+            katSayisi = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Balkon sayısını giriniz: ");
+            balkonSayisi = Convert.ToInt32(Console.ReadLine());
+            AylikOdeme(cephe, balkonSayisi, daireDurumu);
+            
+        }
+
+        public void AylikOdeme(string cephe,int banyoSayisi,string daireDurumu)
+        {
+            if(cephe=="güney" && banyoSayisi > 3)
+            {
+                fiyat = fiyat + aidat + yoneticiUcret;
+                Console.WriteLine($"Yıllık ödenecek tutar: {fiyat/10}");
+            }else if(cephe=="kuzey" && daireDurumu == "site")
+            {
+                Console.WriteLine("1- Yıllık ödeme");
+                Console.WriteLine("2- Aylık ödeme");
+                int secenek = Convert.ToInt32(Console.ReadLine());
+                switch (secenek)
+                {
+                    case 1:
+                        
+                        Console.WriteLine($"Yıllık ödenecek tutar: {fiyat / 10}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Aylık ödenecek tutar: {fiyat / 120}");
+                        break;
+                }
+            }
+        }
+
+        public override void Teklifler(double fiyat)
+        {
+            Console.WriteLine("0- Boş");
+            Console.WriteLine("1- Bahçe");
+            Console.WriteLine("2- Ebeveyn Banyosu");
+            Console.WriteLine("3- Şömine");
+            Console.Write("Seçim yapınız: ");
+            int secim = Convert.ToInt32(Console.ReadLine());
+
+            switch (secim)
+            {
+                case 0:
+                    Console.WriteLine($"Dairenizin fiyatı: {fiyat}");
+                    break;
+                case 1:
+                    fiyat *= 1.2f;
+                    Console.WriteLine($"Dairenizin fiyatı: {fiyat}");
+                    break;
+                case 2:
+                    fiyat *= 1.03f;
+                    Console.WriteLine($"Dairenizin fiyatı: {fiyat}");
+                    break;
+                case 3:
+                    fiyat *= 1.08f;
+                    Console.WriteLine($"Dairenizin fiyatı: {fiyat}");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ 30032022/30032022/Uygulama2/Ev.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama2
+{
+   abstract class Ev
+    {
+        public int evNo;
+        public string evTipi;
+        public int odaSayisi;
+        public int m2;
+        public int emsalBedel;
+        public int arsaPayi;
+        public string arsaDurumu;
+        public string iskanDurumu;
+        public int aidat;
+        public int yoneticiUcret;
+        public double fiyat;
+
+        public void VeriCek()
+        {
+            Console.Write("Ev no giriniz: ");
+            evNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ev tipini giriniz: ");
+            evTipi = Console.ReadLine();
+            Console.Write("Oda sayısı giriniz: ");
+            odaSayisi = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m2 giriniz: ");
+            m2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Emsal bedel giriniz: ");
+            emsalBedel = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Arsa payını giriniz: ");
+            arsaPayi = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Arsa durumunu giriniz: ");
+            arsaDurumu = Console.ReadLine();
+            Console.Write("İskan durumunu giriniz: ");
+            iskanDurumu = Console.ReadLine();
+            Console.Write("Aidat giriniz: ");
+            aidat = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Yönetici ücretini giriniz: ");
+            yoneticiUcret = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fiyat giriniz: ");
+            fiyat = Convert.ToDouble(Console.ReadLine());
+            Teklifler(fiyat);
+            
+            Console.WriteLine($"Ev zamı: {EvZam(odaSayisi, m2, fiyat)}");
+            Console.WriteLine($"M2 fiyatı: {M2FiyatBul(arsaDurumu, iskanDurumu, m2, fiyat)}");
+            
+        }
+        public abstract void Teklifler(double fiyat);
+        public virtual double EvZam(int odaSayisi, int m2, double fiyat)
+        {
+            if(odaSayisi>10 && m2 > 250)
+            {
+                fiyat *= 1.2f;
+                return fiyat;
+            }
+            else
+            {
+                fiyat *= 1.15f;
+                return fiyat;
+            }
+        }
+
+        public double M2FiyatBul (string arsaDurum,string iskanDurum,double m2,double fiyat)
+        {
+            if (arsaDurum == "var" && iskanDurum == "iskanlı") return fiyat / m2;
+            else
+            {
+                fiyat = fiyat / m2;
+                fiyat *= 0.9f;
+                return fiyat;
+            }
+        }
+    }
+}
