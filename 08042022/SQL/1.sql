40d6c778ddaf2697a2d0a491036931785b453b9a
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-04-08 16:22:07 +0300

    ibrahimbayramli

--- 08042022/SQL/1.sql
+++ 08042022/SQL/1.sql
@@ -2,7 +2,7 @@
 Stored Procedure
 1) Hýz
 
-Stored Procedure oluþturduðumuzda bu sorguyu bir kere çalýþtýrdýðýmýzda sistem derler ve sonrakilerde tekrar tekrar çalýþtýrýlmaz. Daha önce çalýþtýrýldýðýnda hafýzaya alýr ve oradan tekrardan çalýþtýrýr. Normal sorgu çalýþtýrmak istediðimizde ise bunu her seferinde ilk kez karþýlaþmýþ gibi yeniden çalýþtýrýr. Bu da hýz açýsýndan pek avantajlý deðildir.
+Stored Procedure olusturdugumuzda bu sorguyu bir kere calistirdigimizda sistem derler ve sonrakilerde tekrar tekrar çalistirilmaz. Daha once calistirildiðinda hafizaya alir ve oradan tekrardan calýþtirir. Normal sorgu calistirmak istedigimizde ise bunu her seferinde ilk kez karsilasmis gibi yeniden calistirir. Bu da hiz acisindan pek avantajli degildir.
 
 2) Parametre Kullanýmý
 
@@ -11,17 +11,45 @@ Stored Procedure oluþturduðumuzda bu sorguyu bir kere çalýþtýrdýðýmýzda sistem d
 
 --Stored Procedure
 
-create proc Pekle
+alter proc Pekle
+@PersonelNo int,
 @AdSoyad varchar(50),
 @Meslek varchar(50),
-@PersonelNo int,
 @Yas int,
 @Maas money,
 @Prim money,
 @Memleket varchar(50)
 as begin
-insert into Personeller (AdSoyad,Meslek,Yas,Maas,Pirim,Memleket,PersonelNo)
-values(@AdSoyad,@Meslek,@Yas,@Maas,@Prim,@Memleket,@PersonelNo)
+insert into Personeller (PersonelNo,AdSoyad,Meslek,Yas,Maas,Pirim,Memleket)
+values(@PersonelNo,@AdSoyad,@Meslek,@Yas,@Maas,@Prim,@Memleket)
 end
 
-exec Pekle 'Ýbrahim BAYRAMLI','Mühendis',29,25000,9000,'Kars',1
+exec Pekle 100,'Ýbrahim BAYRAMLI','Mühendis',29,25000,9000,'Kars'
+
+--proc veya procedure ikisi de kullanýlabilir.
+create proc VerileriCek 
+as begin
+select * from Personeller
+end
+
+exec VerileriCek
+
+create procedure AramaYap
+@AdSoyad varchar(50)
+as begin
+select * from Personeller where AdSoyad=@AdSoyad
+end
+
+exec AramaYap 'Ýbrahim BAYRAMLI'
+
+--Yasi 25 den buyuk 30 dan kucuk olanlari siralayan procedure
+
+alter proc Sorgula
+@UstSinir int, 
+@AltSinir int
+as begin
+select * from Personeller where Yas<@UstSinir or Yas>@UstSinir
+end 
+
+exec Sorgula 25,30
+
