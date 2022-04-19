0886073b54561066e1f8a6ebeb0daf70e3f5254f
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-04-19 16:33:03 +0300

    ibrahimbayramli

---
+++ 19042022/RentCar/RentCar/Giris.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentCar
+{
+    public partial class Giris : Form
+    {
+        public Giris()
+        {
+            InitializeComponent();
+        }
+
+        private void Giris_Load(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) groupBox1.Visible = true;
+            else groupBox1.Visible = false;
+        }
+    }
+}
