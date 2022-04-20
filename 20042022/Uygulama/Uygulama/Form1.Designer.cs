69b18158e514877e1b97d25d7f426f2ec35fefad
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-04-20 16:14:45 +0300

    ibrahimbayramli

---
+++ 20042022/Uygulama/Uygulama/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Uygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ürün ve Tedarikçi Bilgileri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 57);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Birim Fiyata Göre ASC Sıralama";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(283, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 57);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Ürün Adına Göre DESC Sıralama";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(418, 318);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 57);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Ürün ve Kategori Bilgileri";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(553, 318);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 57);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(688, 318);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 57);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(83, 381);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 57);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(217, 381);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 57);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(351, 381);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 57);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(485, 381);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 57);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(619, 381);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 57);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 277);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ 20042022/Uygulama/Uygulama/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NorthwindEntities islem = new NorthwindEntities();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NorthwindEntities islem = new NorthwindEntities();
+            var sonuc = from urun in islem.Urunlers
+                        join tedarikci in islem.Tedarikcilers on urun.TedarikciID equals tedarikci.TedarikciID
+                        select new
+                        {
+                            urun.UrunAdi,
+                            urun.BirimFiyati,
+                            urun.HedefStokDuzeyi,
+                            tedarikci.SirketAdi,
+                            tedarikci.Sehir,
+                            tedarikci.Telefon
+                        };
+            dataGridView1.DataSource = sonuc.ToList();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in islem.Urunlers orderby urun.BirimFiyati select urun;
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in islem.Urunlers orderby urun.UrunAdi descending select urun;
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in islem.Urunlers
+                        join kategori in islem.Kategorilers on urun.KategoriID equals kategori.KategoriID
+                        select new
+                        {
+                            urun.UrunAdi,
+                            urun.BirimFiyati,
+                            urun.HedefStokDuzeyi,
+                            kategori.KategoriID,
+                            kategori.KategoriAdi,
+                            kategori.Tanimi
+                        };
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+    }
+}
