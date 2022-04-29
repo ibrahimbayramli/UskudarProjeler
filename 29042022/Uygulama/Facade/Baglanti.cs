1aadf575d61b3fcca2f19a562a5a38b198cfd499
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-04-29 15:59:30 +0300

    ibrahimbayramli

---
+++ 29042022/Uygulama/Facade/Baglanti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Facade
+{
+    class Baglanti
+    {
+        public static readonly SqlConnection con = new SqlConnection
+            ("Server=DESKTOP-MO5HLC4\\SQLEXPRESS;Database=getir;Integrated Securiy=true");
+
+    }
+}
