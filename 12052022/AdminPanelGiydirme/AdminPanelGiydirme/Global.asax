25fc46659cc03f7468c877ea1303822ad1279ef2
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-05-12 20:01:09 +0300

    MVC

---
+++ 12052022/AdminPanelGiydirme/AdminPanelGiydirme/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdminPanelGiydirme.MvcApplication" Language="C#" %>
---
+++ 12052022/Uygulama/Uygulama/Controllers/PersonellerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Uygulama.Controllers
+{
+    public class PersonellerController : Controller
+    {
+        private HoldingEntities db = new HoldingEntities();
+
+        // GET: Personeller
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
